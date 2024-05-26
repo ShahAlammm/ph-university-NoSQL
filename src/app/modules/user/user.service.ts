cfc1832995d6d6442ea9b5bfe0ebf764760b330f
Shah Alam <shahalam01660166344@gmail.com>
2024-05-26 22:42:35 +0600

    update code

---
+++ src/app/modules/user/user.service.ts
@@ -0,0 +1,26 @@
+import config from '../../config';
+import { TStudent } from '../student/student.interface';
+import { TUser } from './user.interface';
+import { User } from './user.model';
+
+const createStudentIntoDB = async (password: string, studentData: TStudent) => {
+  const userData: Partial<TUser> = {};
+
+  userData.password = password || (config.default_password as string);
+
+  userData.role = 'student';
+  userData.id = '2030100001';
+
+  const result = await User.create(userData);
+
+  if (Object.keys(result).length) {
+    studentData.id = result.id;
+    studentData.user = result._id;
+  }
+
+  return result;
+};
+
+export const UserServices = {
+  createStudentIntoDB,
+};
