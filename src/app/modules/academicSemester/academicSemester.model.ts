dbe064478b6a656af02f3bdd39420d19d2282374
Shah Alam <shahalam01660166344@gmail.com>
2024-06-05 23:30:40 +0600

    update

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -50,9 +50,7 @@ academicSemesterSchema.pre('save', async function (next) {
   next();
 });
 
-
 export const AcademicSemester = model<TAcademicSemester>(
   'AcademicSemester',
   academicSemesterSchema,
 );
-
