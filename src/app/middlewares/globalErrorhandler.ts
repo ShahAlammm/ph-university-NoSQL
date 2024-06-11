87565fe54b14cce9448774543af9ae7a4c074dbc
Shah Alam <shahalam01660166344@gmail.com>
2024-06-11 23:27:04 +0600

    update

--- src/app/middlewares/globalErrorhandler.ts
+++ src/app/middlewares/globalErrorhandler.ts
@@ -2,6 +2,7 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { NextFunction, Request, Response } from 'express';
 
 const globalErrorHandler = (
@@ -13,10 +14,23 @@ const globalErrorHandler = (
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Something went wrong!';
 
+  type TErrorSources = {
+    path: string | number;
+    message: string;
+  }[];
+
+  const errorSources: TErrorSources = [
+    {
+      path: '',
+      message: '',
+    },
+  ];
+
   return res.status(statusCode).json({
     success: false,
     message,
-    error: err,
+    // error: err,
+    errorSources,
   });
 };
 
